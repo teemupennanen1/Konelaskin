a07a6765bdac3f57beffb503a37c97fa7cc47a27
Teemu Pennanen <teemu.pennanen@helsinki.fi>
2022-12-12 16:36:03 +0200

    Testausta laajennettu

--- src/tests/calculation_test.py
+++ src/tests/calculation_test.py
@@ -1,6 +1,6 @@
 '''Test module for the calculations'''
 import unittest
-from src.services.calculate import Calculate
+from services.calculate import Calculate
 
 class TestCalculation(unittest.TestCase):
     '''This class is for testing the programs'''
@@ -23,3 +23,23 @@ class TestCalculation(unittest.TestCase):
     def test_division_operator_returns_correct_value_when_natural_number(self):
         '''Test for division operation'''
         self.assertEqual(self.calculate.calculate(self.parts, "/"), 1.0)
+
+    def test_first_actor_can_be_negative(self):
+        '''Test for the case when the first actor is negative'''
+        self.assertEqual(self.calculate.calculate([-2,2], "+"), 0.0)
+
+    def test_multiple_plus_operators_return_right_answer(self):
+        '''Test for multiple plus operator'''
+        self.assertEqual(self.calculate.calculation_logic("2++2="), 4.0)
+
+    def test_multiple_minus_operators_return_right_answer(self):
+        '''Test for multiple minus operators'''
+        self.assertEqual(self.calculate.calculation_logic("2--2="), 4.0)
+
+    def test_odd_number_of_minus_operators_recognized_as_negative(self):
+        '''Test for odd number of minus operators'''
+        self.assertEqual(self.calculate.calculation_logic("2-+2="), 0.0)
+
+    def test_even_number_of_minus_operators_recognized_as_negative(self):
+        '''Test for even number of minus operators'''
+        self.assertEqual(self.calculate.calculation_logic("2--2="), 4.0)
